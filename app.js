644ab78ab30dcd38ece12da1121987971e23dfa4
Justin Lowe <justin.lowe53@yahoo.com>
2023-10-01 13:13:55 -0400

    inital commit backend for insurance lady angular

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const MessageRoutes = require("./routes/Message.routes");
+const ConsultationRoutes = require("./routes/Consulation.routes");
+const path = require("path");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/message", MessageRoutes);
+app.use("/api/consulation", ConsultationRoutes);
+
+app.use(express.static(path.join(__dirname, "dist")));
+app.use("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+app.listen(3000 || process.env.PORT);
