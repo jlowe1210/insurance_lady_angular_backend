8d1c931073468b0bd3b2b711554d74674bd7a948
Justin Lowe <justin.lowe53@yahoo.com>
2023-10-02 00:12:05 -0400

    testing

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const MessageRoutes = require("./routes/Message.routes");
+const ConsultationRoutes = require("./routes/Consulation.routes");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/message", MessageRoutes);
+app.use("/api/consulation", ConsultationRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).send("testing");
