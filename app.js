a8553fa8651e8abb42200680a849a8e34255c365
Justin Lowe <justin.lowe53@yahoo.com>
2023-10-02 00:05:10 -0400

    testing

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ const path = require("path");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, "dist")));
-app.use("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+app.get("/", (req, res) => {
+  res.send("testing");
 });
 
 const PORT = 3000 || process.env.PORT;
