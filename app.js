45582278bdfbc8620a3c4d9b27fd681fcc047f1f
Justin Lowe <justin.lowe53@yahoo.com>
2023-10-02 00:07:14 -0400

    testing

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 const express = require("express");
 const app = express();
-const path = require("path");
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("testing");
